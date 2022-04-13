61ad7f954ea609270334ef8edfeed61eef84e46f
student <student@LAB5-07>
2022-04-13 09:57:28 +0300

    auth page

--- classmaster/MainWindow.xaml.cs
+++ classmaster/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using classmaster.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace classmaster
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new Auth());
         }
     }
 }
---
+++ classmaster/Pages/Auth.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace classmaster.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : Page
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if ((sender as TextBox).Text == "Введите Логин")
+            {
+                (sender as TextBox).Text = "";
+
+            }
+        }
+
+        private void TextBox_GotFocus_1(object sender, RoutedEventArgs e)
+        {
+            if ((sender as TextBox).Text == "Введите Пароль")
+            {
+                (sender as TextBox).Text = "";
+
+            }
+        }
+    }
+}
